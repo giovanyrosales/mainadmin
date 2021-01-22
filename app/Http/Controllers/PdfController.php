132b7e161e65a4d0e64acaf14250b704d57e106f
giovanyrosales <gioer21@gmail.com>
2021-01-22 12:59:23 -0600

    add clasificacion en materiales

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -69,6 +69,7 @@ class PdfController extends Controller
             // Actualizar saldo de cuenta bolson.
             $nuevosaldo = floatval($bolson->saldo) - floatval($proyecto->monto);
             DB::table('bolson')->where('id', '=', $bolson->id)->update(['saldo' => $nuevosaldo ]);
+            DB::table('proyecto')->where('id', '=', $proyecto->id)->update(['estado' => "1"]);
         }
       $pdf = PDF::loadView('backend.reportes.reforma_apertura', compact('proyecto','materiales', 'cuenta', 'total', 'linean', 'fuentern', 'fuentefn', 'areagestionn'));
       $pdf->setPaper('letter', 'portrait')->setWarnings(false);
